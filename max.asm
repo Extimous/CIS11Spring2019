faa6273c0b369e2fa80ddb2c284af16e94f06b3b
conocolo <44531111+conocolo@users.noreply.github.com>
2019-05-28 19:48:28 -0700

    Boilerplate max function code

---
+++ max.asm
@@ -0,0 +1,12 @@
+; max.asm
+; Test scores will be loaded into R1 to R5
+; Program will check every one to determine which one is greatest
+
+.ORIG x3000
+; Load data into registers
+
+; Main loop
+
+; Variables
+
+.END
