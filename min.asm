9d7e45ac4cf2e72434829feedaae1d176dbbb8a0
Extimous <Extimous@gmail.com>
2019-05-30 18:31:11 -0700

    Update min.asm

--- min.asm
+++ min.asm
@@ -1,68 +1,4 @@
 .ORIG x3000
-
-LDI R1, TS1
-LDI R2, TS2
-LDI R3, TS3
-LDI R4, TS4
-LDI R5, TS5
-
-FINDMIN
-  ADD R1, R1, #-1
-  BRz GIVEMIN1
-  ADD R2, R2, #-1
-  BRz GIVEMIN2
-  ADD R3, R3, #-1
-  BRz GIVEMIN3
-  ADD R4, R4, #-1
-  BRz GIVEMIN4
-  ADD R5, R5, #-1
-  BRz GIVEMIN5
-  BR FINDMIN
-  
-GIVEMIN1
-  STI R1, MIN
-  PUTS MINZ
-GIVEMIN2
-  STI R2, MIN
-  PUTS MINZ
-GIVEMIN3
-  STI R3, MIN
-  PUTS MINZ
-GIVEMIN4
-  STI R4, MIN
-  PUTS MINZ
-GIVEMIN5
-  STI R5, MIN
-  PUTS MINZ
-  PUT MIN
-
-TS1     .FILL x3200
-TS2     .FILL x3201
-TS3     .FILL x3202
-TS4     .FILL x3203
-TS5     .FILL x3204
-MIN     .FILL x3205
-MAX     .FILL x3206
-AVG     .FILL x3207
-
-MINZ   .STRINGZ "The minimum value is: "
-MAXZ   .STRINGZ "The maximum value is: "
-AVGZ   .STRINGZ "The average value is: "
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.ORIG x3000
 LDI R1, TS1
 LDI R2, TS2
 LDI R3, TS3
@@ -166,4 +102,55 @@ AVGZ   .STRINGZ "The average value is: "
 
 
 
-
+;Experimental MIN procedure
+;non-operational
+;.ORIG x3000
+;
+;LDI R1, TS1
+;LDI R2, TS2
+;LDI R3, TS3
+;LDI R4, TS4
+;LDI R5, TS5
+;
+;FINDMIN
+;  ADD R1, R1, #-1
+;  BRz GIVEMIN1
+;  ADD R2, R2, #-1
+;  BRz GIVEMIN2
+;  ADD R3, R3, #-1
+;  BRz GIVEMIN3
+;  ADD R4, R4, #-1
+;  BRz GIVEMIN4
+;  ADD R5, R5, #-1
+;  BRz GIVEMIN5
+;  BR FINDMIN
+;  
+;GIVEMIN1
+;  STI R1, MIN
+;  PUTS MINZ
+;GIVEMIN2
+;  STI R2, MIN
+;  PUTS MINZ
+;GIVEMIN3
+;  STI R3, MIN
+;  PUTS MINZ
+;GIVEMIN4
+;  STI R4, MIN
+;  PUTS MINZ
+;GIVEMIN5
+;  STI R5, MIN
+;  PUTS MINZ
+;  PUT MIN
+;
+;TS1     .FILL x3200
+;TS2     .FILL x3201
+;TS3     .FILL x3202
+;TS4     .FILL x3203
+;TS5     .FILL x3204
+;MIN     .FILL x3205
+;MAX     .FILL x3206
+;AVG     .FILL x3207
+;
+;MINZ   .STRINGZ "The minimum value is: "
+;MAXZ   .STRINGZ "The maximum value is: "
+;AVGZ   .STRINGZ "The average value is: "
